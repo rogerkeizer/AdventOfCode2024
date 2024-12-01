f234316e4241fd2f45904125e8a7849fa1e81ec5
Roger <roger@1bits.cloud>
2024-12-01 11:19:49 +0100

    solution day 1

---
+++ Day01/ParseResult.cs
@@ -0,0 +1,8 @@
+namespace AdventOfCode2024
+{
+    class ParseResult()
+    {
+        public List<int> left {get;set;} = new List<int>();
+        public List<int> right {get;set;} = new List<int>();
+    }
+}
---
+++ Day01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var solution = new Solution("input.txt");
+            
+            Console.WriteLine($"start 1: {solution.Star1()}");
+            
+            Console.WriteLine($"start 2: {solution.Star2()}");
+        }
+    }
+}
